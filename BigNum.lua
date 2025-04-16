f64a1b76e16b43917cdcb8b8fdef2d95c4d7101f
DataStoreGen <lk6394785@gmail.com>
2025-04-16 15:03:43 -0800

    Update BigNum.lua
    a change

--- BigNum.lua
+++ BigNum.lua
@@ -2,20 +2,23 @@ type BigNum = {number}
 local BigNum = {}
 
 function BigNum.new(man: number, exp: number): BigNum
-	while math.abs(man) >= 10 do
-		man/=10
+	while man >= 1000 do
+		man/=1000
 		exp+=1
 	end
-	while math.abs(man) < 1 and man ~= 0 do
+	while man < 1 and man ~= 0 do
 		man*=10
 		exp-=1
 	end
+	if man == 0/0 then return {-2, 0}end
+	if exp == math.huge or exp == -math.huge then return {1, math.huge} end
 	return {man, math.floor(exp)}
 end
 
 function BigNum.fromNumber(value: number): BigNum
-	local exp = math.log10(value)
-	return {value/10^exp, exp}
+	local exp = math.floor(math.log10(math.abs(value)))
+	local man = value/(10^exp)
+	return BigNum.new(man, exp)
 end
 
 function BigNum.fromString(value: string): BigNum
@@ -40,15 +43,24 @@ function BigNum.convert(value): BigNum
 		local man, exp = string.match(value, '([-+%d%.]+)[eE]([-+]?%d+)')
 		if man and exp then
 			man, exp = tonumber(man), tonumber(exp)
-			return {man, exp}
+			return BigNum.new(man, exp)
 		end
 		warn('Failed to find e or convert "" into BigNum')
 		return {0, 0}
 	elseif type(value) == 'number' then
-		local exp = math.log10(value)
-		return {value/10^exp, exp}
+		local exp = math.floor(math.log10(value))
+		local man = value/10^exp
+		return BigNum.new(man, exp)
 	elseif type(value) == 'table' then
-		return {value[1], value[2]}
+		if #value == 2 then
+			return BigNum.new(value[1], value[2])
+		elseif #value == 1 then
+			local exp = math.floor(math.log10(value))
+			local man = value/10^exp
+			return BigNum.new(man, exp)
+		else
+			return value
+		end
 	end
 	warn('Failed to convert to BigNum')
 	return {0, 0}
@@ -67,6 +79,11 @@ function BigNum.fdiv(val1, val2)
 end
 
 function BigNum.toString(value: BigNum): string
+	if value[1] == -2 then return 'NaN' end
+	if value[1] == 0 then return '0e0' end
+	if value[2] == math.huge then
+		return (if value[2] == -1 then "-" else "") .. "Inf"
+	end
 	return value[1] .. 'e' .. value[2]
 end
 
@@ -138,14 +155,16 @@ end
 
 function BigNum.pow(val1, val2): BigNum
 	val1, val2 = BigNum.convert(val1), BigNum.convert(val2)
-	local man1, exp1 = val1[1], val1[2]
-	local man2, exp2 = val2[1], val2[2]
-	local man = man1^man2
-	local exp = exp1*10^exp2*man2
-	return BigNum.new(man ,exp)
+	local lg10 = math.log10(val1[1]) + val1[2]
+	local exponent = val2[1] * 10^val2[2]
+	local rlog = lg10 * exponent
+	local exp = math.floor(rlog)
+	local man = 10 ^ (rlog - exp)
+	return BigNum.new(man, exp)
 end
 
 function BigNum.log(val1, val2): BigNum
+	val2 = val2 or 2.718281828459045
 	val1, val2 = BigNum.convert(val1), BigNum.convert(val2)
 	local man1, exp1 = val1[1], val1[2]
 	local man2, exp2 = val2[1], val2[2]
@@ -156,11 +175,12 @@ function BigNum.log(val1, val2): BigNum
 end
 
 function BigNum.log10(val): BigNum
-	return BigNum.log(val, 10)
+	val = BigNum.convert(val)
+	local man, exp = val[1], val[2]
+	return BigNum.fromNumber(math.log10(man)+exp)
 end
 
 function BigNum.toNumber(val: BigNum): number
-	val = BigNum.convert(val)
 	return val[1]*10^val[2]
 end
 
@@ -180,32 +200,34 @@ function BigNum.root(val1, val2)
 	return BigNum.new(man1^(1/root), exp1/root)
 end
 
+function BigNum.slog10(val)
+	val = BigNum.convert(val)
+	local count = 0
+	while val[2] > 1 do
+		val = BigNum.log10(val)
+		count += 1
+	end
+	local frac = (math.log10(val[1]) + val[2]) / math.log10(10)
+	return BigNum.fromNumber(count + frac)
+end
+
+function BigNum.plog10(val1, val2)
+	return BigNum.log10(BigNum.pow(val1, val2))
+end
+
+function BigNum.plog(val1, val2, log)
+	return BigNum.log(BigNum.pow(val1, val2), log)
+end
+
+function BigNum.p10log10(val)
+	return BigNum.log10(BigNum.pow10(val))
+end
+
 function BigNum.mod(val1, val2): BigNum
 	val1, val2 = BigNum.convert(val1), BigNum.convert(val2)
 	local result = BigNum.sub(val1, BigNum.mul(val2, BigNum.fdiv(val1, val2)))
 	if result[1] == 0 then return {0,0} end
 	return BigNum.new(val1[1]%val2[1], 0)
-end
-
-local letterTable = {
-	'','a', 'b', 'c' ,'d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'
-}
-function BigNum.letterShort(val, digits): string
-	val = BigNum.convert(val)
-	local man, exp = val[1], val[2]
-	local group = math.floor(exp / 3)
-	local letters = ""
-	if group == 0 then
-		letters = "a"
-	else
-		while group > 0 do
-			local index = (group - 1) % 26 + 1
-			letters = letterTable[index+1] .. letters
-			group = math.floor((group - 1) / 26)
-		end
-	end
-	local lf = math.fmod(exp, 3)
-	return BigNum.showDigits(man * 10^lf, digits) .. letters
 end
 
 function BigNum.chechStatus(exp1, exp2, man1, man2)
@@ -277,10 +299,12 @@ end
 function BigNum.short(val, digits, canComma: boolean?): string
 	canComma = canComma or false
 	val = BigNum.convert(val)
+	if val[1] == -2 then return "NaN" end 
+	if val[2] == 1e309 then return "inf" end 
 	local SNumber1: number, SNumber: number = val[1], val[2]
 	local leftover = math.fmod(SNumber, 3)
 	SNumber = math.floor(SNumber / 3)-1
-	if SNumber <= -1 then return tostring(BigNum.showDigits(SNumber1 * 10^leftover, digits)) end	
+	if SNumber <= -1 then return tostring(BigNum.showDigits(SNumber1 * (10^leftover) + 0.001, digits)) end	
 	local FirBigNumOnes: {string} = {"", "U","D","T","Qd","Qn","Sx","Sp","Oc","No"}
 	local SecondOnes: {string} = {"", "De","Vt","Tg","qg","Qg","sg","Sg","Og","Ng"}
 	local ThirdOnes: {string} = {"", "Ce", "Du","Tr","Qa","Qi","Se","Si","Ot","Ni"}
@@ -289,15 +313,15 @@ function BigNum.short(val, digits, canComma: boolean?): string
 		if SNumber == 0 or SNumber == 1 then
 			return BigNum.AddComma(val)
 		elseif SNumber == 2 then
-			return tostring(BigNum.showDigits(SNumber1 * 10^leftover, digits)) .. "b"
+			return tostring(BigNum.showDigits(SNumber1 * (10^leftover) +0.001, digits)) .. "b"
 		end
 	else
 		if SNumber == 0 then
-			return tostring(BigNum.showDigits(SNumber1 * 10^leftover, digits)) .. "k"
+			return tostring(BigNum.showDigits(SNumber1 * (10^leftover) +0.001, digits)) .. "k"
 		elseif SNumber == 1 then 
-			return tostring(BigNum.showDigits(SNumber1 * 10^leftover, digits)) .. "m"
+			return tostring(BigNum.showDigits(SNumber1 * (10^leftover) +0.001, digits)) .. "m"
 		elseif SNumber == 2 then
-			return tostring(BigNum.showDigits(SNumber1 * 10^leftover, digits)) .. "b"
+			return tostring(BigNum.showDigits(SNumber1 * (10^leftover) +0.001, digits)) .. "b"
 		end
 	end
 	local txt: string = ""
@@ -329,7 +353,7 @@ function BigNum.short(val, digits, canComma: boolean?): string
 	end
 	if SNumber < 1000 then
 		suffixpart(SNumber)
-		return tostring(BigNum.showDigits(SNumber1 * 10^leftover, digits)) .. txt
+		return tostring(BigNum.showDigits(SNumber1 * (10^leftover) +0.001, digits)) .. txt
 	end
 	for i=#MultOnes,0,-1 do
 		if SNumber >= 10^(i*3) then
@@ -338,7 +362,7 @@ function BigNum.short(val, digits, canComma: boolean?): string
 			SNumber = math.fmod(SNumber, 10^(i*3))
 		end
 	end
-	return tostring(BigNum.showDigits(SNumber1 * 10^leftover, digits)) .. txt
+	return tostring(BigNum.showDigits(SNumber1 * (10^leftover) +0.001, digits)) .. txt
 end
 
 function BigNum.shortE(val, digits): string
@@ -353,14 +377,14 @@ function BigNum.shortE(val, digits): string
 		return (first[one+1] or '') .. (second[ten+1] or '') .. (third[hun+1] or '')
 	end
 	local man, exp = val[1], val[2]
-	local lf = math.fmod(exp, 3)
+	local lf = math.fmod(math.floor(exp), 3)
 	local index = 0
-	exp = math.floor(exp)
 	while exp >= 1e3 do
 		exp/=1e3
 		index +=1
 	end
-	man = BigNum.showDigits(man * 10^lf, digits)
+	man = BigNum.showDigits(man^lf + 0.001, digits)
+	exp = math.floor(exp* 100 + 0.001) / 100
 	if index == 1 then
 		return man .. 'e' .. exp .. 'k'
 	elseif index == 2 then
@@ -371,15 +395,15 @@ function BigNum.shortE(val, digits): string
 	return man .. 'e' .. exp ..suffixPart(index)
 end
 
-function BigNum.HyperE(val): string
+function BigNum.HyperE(val, digit: number?): string
 	val = BigNum.convert(val)
 	local man, exp = val[1], val[2]
-	if math.fmod(exp, 1000) then
-		local newExp = math.floor(math.log10(exp))
-		exp /=10^newExp
-		return man .. 'e' .. exp .. 'e' .. newExp
-	end
-	return man ..'e' .. exp
+	local newExp = math.floor(math.log10(exp))
+	local lfe = math.fmod(exp, 3)
+	exp /= 10^newExp
+	man = BigNum.showDigits(man* 10^ lfe, digit)
+	exp = BigNum.showDigits(exp*10 ^lfe, digit)
+	return man .. 'e' .. exp .. 'e' ..  newExp
 end
 
 function BigNum.AddComma(val): string
@@ -401,13 +425,6 @@ function BigNum.fshortE(val, digit): string
 		return '1/' .. BigNum.shortE(BigNum.div(1, val), digit)
 	end
 	return BigNum.shortE(val, digit)
-end
-
-function BigNum.fLetter(val, digit): string
-	if BigNum.between(val, 0, 1) then
-		return '1/' .. BigNum.letterShort(BigNum.div(1, val), digit)
-	end
-	return BigNum.letterShort(val, digit)
 end
 
 function BigNum.fHyperE(val): string
@@ -472,24 +489,77 @@ end
 
 function BigNum.lbencode(val): number
 	val = BigNum.convert(val)
-	local a = BigNum.add({1, val[2]}, 1)
-	if a[2] ~= a[2] then return 0 end 
-	local exp = a[2]
+	local man, exp = val[1], val[2]
 	if exp > 1.7976931348623157e308 then
 		exp = 1.7976931348623157e308
 	end
-	if a[1] == 0 or exp <= 0 then return 0 end
-	return (math.log10(exp + 1) + 1) * 4503599627370496 * val[1]
+	local compress = (math.log10(exp+1)+1)*4503599627370496
+	return compress
 end
 
 function BigNum.lbdecode(val: number): BigNum
-	if val == 0 then return {0,0} end
+	if val == 0 then return {0, 0} end
 	local s = math.sign(val)
 	val = math.abs(val)
-	local toBig = {1, 10^(val/4503599627370496-1)-1}
-	toBig = BigNum.sub(toBig, 1)
-	toBig[2] = math.floor(toBig[2] * 100 + 0.001) / 100
-	return {s, toBig[2]}
+	local exp = 10^(val/4503599627370496-1)-1
+	local man = val/((math.log10(exp+1)+1) * 4503599627370496)
+	return BigNum.new(man, exp)
+end
+
+function BigNum.Combine(val, digit, canComma): string
+	if BigNum.meeq(val, {1, 1e100}) then
+		return BigNum.HyperE(val)
+	elseif BigNum.meeq(val, {1, 1e10}) then
+		return BigNum.shortE(val, digit)
+	else return BigNum.short(val, digit, canComma)
+	end
+end
+
+function BigNum.abs(val): BigNum
+	val = BigNum.convert(val)
+	local man, exp = val[1], val[2]
+	if man < 0 then
+		man=-man
+	elseif exp < 0 then
+		exp=-exp
+	end
+	return {man, exp}
+end
+
+function BigNum.buy1(cost, pow): BigNum
+	return  BigNum.mul(cost, pow)
+end
+
+function BigNum.buy5(cost, pow): BigNum
+	return BigNum.mul(cost, BigNum.pow(pow, 5))
+end
+
+function BigNum.buy10(cost, pow): BigNum
+	return BigNum.mul(cost, BigNum.pow10(pow))
+end
+
+function BigNum.rlog(val1, val2, base): BigNum
+	return BigNum.log(BigNum.root(val1, val2), base)
+end
+
+function BigNum.rlog10(val1, val2): BigNum
+	return BigNum.log10(BigNum.root(val1, val2))
+end
+
+function BigNum.proot10(val1, val2)
+	return BigNum.root(BigNum.pow(val1, val2), 10)
+end
+
+function BigNum.proot(val1, val2, root)
+	return BigNum.root(BigNum.pow(val1, val2), root)
+end
+
+function BigNum.rootp(val1, val2, pow)
+	return BigNum.pow(BigNum.root(val1, val2), pow)
+end
+
+function BigNum.rootp10(val1, val2)
+	return BigNum.pow(BigNum.root(val1, val2), 10)
 end
 
 return BigNum
